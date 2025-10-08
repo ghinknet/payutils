45736a2f2c5de1b3b8fda57ef197bc895879c327
Narika <drnarika@outlook.com>
2025-10-08 16:21:20 +0000

    Update wechat callback method and model

--- internal/controller/wechat/gin.go
+++ internal/controller/wechat/gin.go
@@ -1,8 +1,12 @@
 package wechat
 
 import (
+	"net/http"
+
 	"git.ghink.net/ghink/payutils/internal/model"
 	"github.com/gin-gonic/gin"
+	"github.com/go-pay/gopay"
+	"github.com/go-pay/gopay/wechat/v3"
 )
 
 type GinController struct {
@@ -15,7 +19,56 @@ func (g *GinController) Create(c *gin.Context) {
 }
 
 func (g *GinController) Callback(c *gin.Context) {
+	notifyReq, err := wechat.V3ParseNotify(c.Request)
+	if err != nil {
+		return
+	}
 
+	//TODO:这玩意到底能不能跑？
+	// 获取微信平台证书
+	certMap := g.Client.Wechat.WxPublicKeyMap()
+	// 验证异步通知的签名
+	err = notifyReq.VerifySignByPKMap(certMap)
+	if err != nil {
+		return
+	}
+
+	// 微信消息解密
+	wechatPayCallback := &model.WechatPayCallback{}
+	err = notifyReq.DecryptCipherTextToStruct(
+		g.Config.WeChatPay.MerchantAPIv3Key, *wechatPayCallback)
+	if err != nil {
+		return
+	}
+
+	//TODO:这四个状态是不是要修改model
+	var status model.TradeStatus
+	switch wechatPayCallback.TradeState {
+	case model.WechatTradeStateSuccess:
+		status = model.TradeSuccess
+	case model.WechatTradeStateClosed:
+		status = model.TradeClosed
+	case model.WechatTradeStateNotPay:
+		status = model.TradePending
+	case model.WechatTradeStateRefund:
+		status = model.TradeClosed
+	}
+	// Return status
+	err = g.Config.OrderStatus(
+		wechatPayCallback.OutTradeNo,
+		c.Request.Header.Get("Authorization"),
+		status,
+	)
+	if err != nil {
+		return
+	}
+
+	// ====↓↓↓====异步通知应答====↓↓↓====
+	// 退款通知http应答码为200且返回状态码为SUCCESS才会当做商户接收成功，否则会重试。
+	// 注意：重试过多会导致微信支付端积压过多通知而堵塞，影响其他正常通知。
+
+	// 此写法是 gin 框架返回微信的写法
+	c.JSON(http.StatusOK, &wechat.V3NotifyRsp{Code: gopay.SUCCESS, Message: "成功"})
 }
 
 func (g *GinController) OpenIDCallback(c *gin.Context) {
---
+++ internal/model/wechat.go
@@ -0,0 +1,79 @@
+package model
+
+// WechatTradeState provides enum type for wechat trade status const
+type WechatTradeState string
+
+const (
+	WechatTradeStateSuccess  WechatTradeState = "SUCCESS"    // 支付成功
+	WechatTradeStateRefund   WechatTradeState = "REFUND"     // 转入退款
+	WechatTradeStateNotPay   WechatTradeState = "NOTPAY"     // 未支付
+	WechatTradeStateClosed   WechatTradeState = "CLOSED"     // 已关闭
+	WechatTradeStateRevoked  WechatTradeState = "REVOKED"    // 已撤销（仅付款码支付会返回）
+	WechatTradeStatePaying   WechatTradeState = "USERPAYING" // 用户支付中（仅付款码支付会返回）
+	WechatTradeStatePayError WechatTradeState = "PAYERROR"   // 支付失败（仅付款码支付会返回）
+)
+
+// WechatTradeType provides enum type for wechat trade type const
+type WechatTradeType string
+
+const (
+	WechatTradeTypeApp      WechatTradeType = "APP"      // APP支付
+	WechatTradeTypeJSAPI    WechatTradeType = "JSAPI"    // JSAPI支付
+	WechatTradeTypeNative   WechatTradeType = "NATIVE"   // Native支付
+	WechatTradeTypeH5       WechatTradeType = "MWEB"     // H5支付
+	WechatTradeTypeMicropay WechatTradeType = "MICROPAY" // 付款码支付
+	WechatTradeTypeFacepay  WechatTradeType = "FACEPAY"  // 刷脸支付
+)
+
+type WechatPayCallback struct {
+	TransactionID   string            `json:"transaction_id"`
+	Amount          AmountInfo        `json:"amount"`
+	MchID           string            `json:"mchid"`
+	TradeState      WechatTradeState  `json:"trade_state"`
+	BankType        string            `json:"bank_type"`
+	PromotionDetail []PromotionDetail `json:"promotion_detail,omitempty"`
+	SuccessTime     string            `json:"success_time"`
+	Payer           PayerInfo         `json:"payer"`
+	OutTradeNo      string            `json:"out_trade_no"`
+	AppID           string            `json:"appid"`
+	TradeStateDesc  string            `json:"trade_state_desc"`
+	TradeType       WechatTradeType   `json:"trade_type"`
+	Attach          string            `json:"attach,omitempty"`
+	SceneInfo       SceneInfo         `json:"scene_info,omitempty"`
+}
+
+type AmountInfo struct {
+	PayerTotal    int    `json:"payer_total"`
+	Total         int    `json:"total"`
+	Currency      string `json:"currency"`
+	PayerCurrency string `json:"payer_currency"`
+}
+
+type PromotionDetail struct {
+	Amount              int           `json:"amount"`
+	WechatpayContribute int           `json:"wechatpay_contribute"`
+	CouponID            string        `json:"coupon_id"`
+	Scope               string        `json:"scope"`
+	MerchantContribute  int           `json:"merchant_contribute"`
+	Name                string        `json:"name"`
+	OtherContribute     int           `json:"other_contribute"`
+	Currency            string        `json:"currency"`
+	StockID             string        `json:"stock_id"`
+	GoodsDetail         []GoodsDetail `json:"goods_detail,omitempty"`
+}
+
+type GoodsDetail struct {
+	GoodsRemark    string `json:"goods_remark"`
+	Quantity       int    `json:"quantity"`
+	DiscountAmount int    `json:"discount_amount"`
+	GoodsID        string `json:"goods_id"`
+	UnitPrice      int    `json:"unit_price"`
+}
+
+type PayerInfo struct {
+	OpenID string `json:"openid"`
+}
+
+type SceneInfo struct {
+	DeviceID string `json:"device_id"`
+}
