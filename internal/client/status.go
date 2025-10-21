6877457cfbe1646113d48450c1057fa830e98b30
Bigsk <i@ianxia.com>
2025-10-21 20:18:34 +0800

    Check client before check status to avoid nil pointer

--- internal/client/status.go
+++ internal/client/status.go
@@ -16,32 +16,36 @@ type Client struct {
 
 // CheckStatus checks whether an order has been paid
 func (c *Client) CheckStatus(orderID string) (bool, error) {
-	// Check WeChat-Pay
-	wxRsp, err := c.WeChat.V3TransactionQueryOrder(context.Background(), 2, orderID)
-	if err != nil {
-		return false, err
-	}
-	if wxRsp.Code != 0 && wxRsp.Code != 404 {
-		return false, model.ErrWeChatPayRespCodeInvalid
-	}
-	if wxRsp.Response.TradeState == "SUCCESS" {
-		return true, nil
+	if c.WeChat != nil {
+		// Check WeChat-Pay
+		wxRsp, err := c.WeChat.V3TransactionQueryOrder(context.Background(), 2, orderID)
+		if err != nil {
+			return false, err
+		}
+		if wxRsp.Code != 0 && wxRsp.Code != 404 {
+			return false, model.ErrWeChatPayRespCodeInvalid
+		}
+		if wxRsp.Response.TradeState == "SUCCESS" {
+			return true, nil
+		}
 	}
 
-	// Prepare params
-	bm := make(gopay.BodyMap)
-	bm.Set("out_trade_no", orderID)
+	if c.Alipay != nil {
+		// Prepare params
+		bm := make(gopay.BodyMap)
+		bm.Set("out_trade_no", orderID)
 
-	// Check Alipay
-	aliRsp, err := c.Alipay.TradeQuery(context.Background(), bm)
-	if err != nil {
-		return false, err
-	}
-	if aliRsp.StatusCode != http.StatusOK && aliRsp.ErrResponse.Code != "ACQ.TRADE_NOT_EXIST" {
-		return false, model.ErrAlipayRespCodeInvalid
-	}
-	if aliRsp.TradeStatus == "TRADE_SUCCESS" {
-		return true, nil
+		// Check Alipay
+		aliRsp, err := c.Alipay.TradeQuery(context.Background(), bm)
+		if err != nil {
+			return false, err
+		}
+		if aliRsp.StatusCode != http.StatusOK && aliRsp.ErrResponse.Code != "ACQ.TRADE_NOT_EXIST" {
+			return false, model.ErrAlipayRespCodeInvalid
+		}
+		if aliRsp.TradeStatus == "TRADE_SUCCESS" {
+			return true, nil
+		}
 	}
 
 	return false, nil
