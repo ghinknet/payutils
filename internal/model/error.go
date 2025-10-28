fa0b4c8de5a40a2107fac9156228c550405a639c
Narika <drnarika@outlook.com>
2025-10-28 17:42:37 +0000

    correct ErrMissAllowedOrigin text

--- internal/model/error.go
+++ internal/model/error.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 )
 
-var ErrMissAllowedOrigin = errors.New("miss endpoint")
+var ErrMissAllowedOrigin = errors.New("miss allowedorigin")
 var ErrMissEndpoint = errors.New("miss endpoint")
 var ErrMissOrderHandler = errors.New("miss order handler")
 var ErrWeChatPayRespCodeInvalid = errors.New("wechat pay resp code invalid")
